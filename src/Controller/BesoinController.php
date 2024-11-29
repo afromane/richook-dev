93164e5ccaf2dc2432f29bbcebd70183c3ecc053
Manel Issa <issamanel45@gmail.com>
2024-11-29 13:58:41 +0100

    update code

--- src/Controller/BesoinController.php
+++ src/Controller/BesoinController.php
@@ -2,25 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Besoin;
 use App\Form\BesoinType;
+use App\Service\FileUploader;
+use App\Repository\UserRepository;
 use App\Repository\BesoinRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Service\FileUploader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/besoin')]
 final class BesoinController extends AbstractController
 {
     #[Route(name: 'app_besoin_index', methods: ['GET'])]
-    public function index(BesoinRepository $besoinRepository): Response
+    public function index(BesoinRepository $besoinRepository,UserRepository $userRepository): Response
     {
+
+        $user = new User();
+        $user = $userRepository->findOneBy(array('email'=>$this->getUser()->getUserIdentifier()));
+
         return $this->render('besoin/index.html.twig', [
-            'besoins' => $besoinRepository->findAll(),
+            'besoins' => $besoinRepository->findBy(array('user'=>$user->getId())),
         ]);
     }
 
